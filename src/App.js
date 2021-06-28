c4e0d5e250d355ce8528d89cc738d6fdb8f08033
Arron Dick <s_drac2@yahoo.com>
2021-06-28 13:14:17 +1200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { FcCalendar } from "react-icons/fc";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container justify-content-center">
+      <h1 className="display-1">
+        <FcCalendar className="inline-block"/>Date check</h1>
     </div>
   );
 }
